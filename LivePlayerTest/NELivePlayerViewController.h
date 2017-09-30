710ef508baa31bd812f3cf1a86db173ab4c2bd2a
zhouhan <zhouhan@zhouhandeMac-mini.local>
2017-09-30 11:40:27 -0700

    add xib

--- LivePlayerTest/NELivePlayerViewController.h
+++ LivePlayerTest/NELivePlayerViewController.h
@@ -8,11 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
+#import <NELivePlayer.h>
+#import <NELivePlayerController.h>
+
 @interface NELivePlayerViewController : UIViewController
 
 @property   (strong,nonatomic) NSURL* url;
 @property   (strong,nonatomic) NSString* mediaType;
 @property   (strong,nonatomic) NSString* decodeType;
+@property   (strong,nonatomic) id<NELivePlayer> liveplayer;
 
 -(instancetype)initWithURL:(NSURL*)url andDecodeParam:(NSMutableArray*)decodeParam;
 @end
--- LivePlayerTest/NELivePlayerViewController.m
+++ LivePlayerTest/NELivePlayerViewController.m
@@ -10,15 +10,18 @@
 
 @interface NELivePlayerViewController ()
 
+@property   (strong,nonatomic) UIView* playerView;
+
 @end
 
 @implementation NELivePlayerViewController
 
 CGFloat screenWidth;
 CGFloat screenHeight;
+BOOL isHardware;
 
 -(instancetype)initWithURL:(NSURL*)url andDecodeParam:(NSMutableArray*)decodeParam{
-    self=[self initWithName:@"NELivePlayerViewController" bundle:nil];
+    self=[self initWithNibName:@"NELivePlayerViewController" bundle:nil];
     if (self) {
         self.url=url;
         self.decodeType=[decodeParam objectAtIndex:0];
@@ -27,7 +30,7 @@ CGFloat screenHeight;
     return  self;
 }
 
--(instancetype)initWithName:(NSString*)nibNameOrNil bundle:(NSBundle*)nibBundleOrNil{
+-(instancetype)initWithNibName:(NSString*)nibNameOrNil bundle:(NSBundle*)nibBundleOrNil{
     self=[super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         
@@ -41,8 +44,42 @@ CGFloat screenHeight;
     screenWidth=CGRectGetWidth([[UIScreen mainScreen] bounds]);
     screenHeight=CGRectGetHeight([[UIScreen mainScreen] bounds]);
     
-}
+    self.playerView=[[UIView alloc] initWithFrame:CGRectMake(0, 0, screenHeight, screenWidth)];
+    
+    [NELivePlayerController setLogLevel:NELP_LOG_DEFAULT];
+    self.liveplayer=[[NELivePlayerController alloc] initWithContentURL:self.url];
+    if (self.liveplayer == nil) {
+        NSLog(@"player initilize failed, please tay again!");
+    }
+    self.liveplayer.view.autoresizingMask=UIViewAutoresizingFlexibleWidth| UIViewAutoresizingFlexibleHeight;
+    self.liveplayer.view.frame=self.playerView.bounds;
+    self.view.autoresizingMask=YES;
+    if ([self.decodeType isEqualToString:@"hardware"]) {
+        isHardware = YES;
+    }
+    else if ([self.decodeType isEqualToString:@"software"]) {
+        isHardware = NO;
+    }
 
+    
+    [self.view addSubview:self.liveplayer.view];
+}
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    if ([self.mediaType isEqualToString:@"livestream"]) {
+        [self.liveplayer setBufferStrategy:NELPLowDelay];
+    }
+    else{
+        [self.liveplayer setBufferStrategy:NELPAntiJitter];
+    }
+    [self.liveplayer setScalingMode:NELPMovieScalingModeNone];
+    [self.liveplayer setShouldAutoplay:YES];
+    [self.liveplayer setHardwareDecoder:isHardware];
+    [self.liveplayer setPauseInBackground:NO];
+    [self.liveplayer setPlaybackTimeout:15*1000];
+    
+    [self.liveplayer  prepareToPlay];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
